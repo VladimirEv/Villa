f32f4bac3023b2ab765e5651f23abec83b7e194c
Vladimir Belevich <vladimir.ev.belevich@gmail.com>
2023-02-23 14:56:24 +0300

    Created first End Point

--- MagicVilla_VillaApi/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaApi/Controllers/VillaAPIController.cs
@@ -1,10 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MagicVilla_VillaApi.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaApi.Controllers
 {
+    [Route("api/VillaAPI")]
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
-
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {           
+            return new List<Villa>
+            {
+                new Villa{Id = 1, Name = "Pool View"},
+                new Villa{Id = 2, Name = "Beach View"}
+            };
+        }
     }
 }
---
+++ MagicVilla_VillaApi/Models/Villa.cs
@@ -0,0 +1,9 @@
+﻿namespace MagicVilla_VillaApi.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
