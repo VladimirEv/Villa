5ce27f7939d9725637f5777228b7aaeec7931988
Vladimir Belevich <vladimir.ev.belevich@gmail.com>
2023-02-23 16:49:28 +0300

    Created new method "GetVilla"

--- MagicVilla_VillaApi/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaApi/Controllers/VillaAPIController.cs
@@ -1,22 +1,25 @@
-﻿using MagicVilla_VillaApi.Models;
+﻿using MagicVilla_VillaApi.Data;
 using MagicVilla_VillaApi.Models.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaApi.Controllers
 {
     //[Route("api/[controller]")]
-    [Route("api/VillaAPI")]
+    //[Route("api/VillaAPI")]
+    [Route("api/[controller]/[action]")]   
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
         public IEnumerable<VillaDTO> GetVillas()
-        {           
-            return new List<VillaDTO>
-            {
-                new VillaDTO{Id = 1, Name = "Pool View"},
-                new VillaDTO{Id = 2, Name = "Beach View"}
-            };
+        {
+            return VillaStore.villaDTOList;
+        }
+
+        [HttpGet]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.villaDTOList.FirstOrDefault(i=>i.Id == id);
         }
     }
 }
---
+++ MagicVilla_VillaApi/Data/VillaStore.cs
@@ -0,0 +1,13 @@
+﻿using MagicVilla_VillaApi.Models.Models.DTO;
+
+namespace MagicVilla_VillaApi.Data
+{
+    public static class VillaStore
+    {
+        public static List<VillaDTO> villaDTOList = new List<VillaDTO>
+            {
+                new VillaDTO{Id = 1, Name = "Pool View"},
+                new VillaDTO{Id = 2, Name = "Beach View"}
+            };
+    }
+}
