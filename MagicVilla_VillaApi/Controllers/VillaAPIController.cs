3546f63260227d1e92d13dca675b576ab03724cd
Vladimir Belevich <vladimir.ev.belevich@gmail.com>
2023-02-24 15:44:20 +0300

    Changed methods "GetVillas" and "GetVilla"

--- MagicVilla_VillaApi/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaApi/Controllers/VillaAPIController.cs
@@ -1,25 +1,38 @@
 ﻿using MagicVilla_VillaApi.Data;
 using MagicVilla_VillaApi.Models.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
 
 namespace MagicVilla_VillaApi.Controllers
 {
-    //[Route("api/[controller]")]
-    //[Route("api/VillaAPI")]
-    [Route("api/[controller]/[action]")]   
+    //[Route("api/[controller]")]    
+    //[Route("api/[controller]/[action]")]
+    [Route("api/VillaAPI")]
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<VillaDTO> GetVillas()
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
-            return VillaStore.villaDTOList;
+            return Ok(VillaStore.villaDTOList);
         }
-
-        [HttpGet]
-        public VillaDTO GetVilla(int id)
+         
+        [HttpGet("{id:int}")]
+        public ActionResult<VillaDTO> GetVilla(int id)
         {
-            return VillaStore.villaDTOList.FirstOrDefault(i=>i.Id == id);
+            if(id==0)
+            {
+                return BadRequest();
+            }
+
+            var villa = VillaStore.villaDTOList.FirstOrDefault(u => u.Id == id);
+
+            if(villa == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(VillaStore.villaDTOList.FirstOrDefault(i=>i.Id == id));
         }
     }
 }
