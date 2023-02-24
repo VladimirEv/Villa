230d6b99eda0c348a0f4ef8810b4a770c6c453fa
Vladimir Belevich <vladimir.ev.belevich@gmail.com>
2023-02-24 22:21:43 +0300

    Validation data in API

--- MagicVilla_VillaApi/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaApi/Controllers/VillaAPIController.cs
@@ -8,7 +8,7 @@ namespace MagicVilla_VillaApi.Controllers
     //[Route("api/[controller]")]    
     //[Route("api/[controller]/[action]")]
     [Route("api/VillaAPI")]
-    [ApiController]
+    //[ApiController]
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
@@ -46,6 +46,11 @@ namespace MagicVilla_VillaApi.Controllers
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villaDTO)
         {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
             if (villaDTO == null) 
             {
                 return BadRequest(villaDTO);
--- MagicVilla_VillaApi/Models/Models.DTO/VillaDTO.cs
+++ MagicVilla_VillaApi/Models/Models.DTO/VillaDTO.cs
@@ -1,9 +1,12 @@
-﻿namespace MagicVilla_VillaApi.Models.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_VillaApi.Models.Models.DTO
 {
     public class VillaDTO //date transfer object
     {
         public int Id { get; set; }
-
+        [Required]
+        [MaxLength(30)]
         public string Name { get; set; }
     }
 }
