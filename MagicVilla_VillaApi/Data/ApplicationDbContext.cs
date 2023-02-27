a292ff9ff22a988218fa78c4a6079301ae6fac55
Vladimir Belevich <vladimir.ev.belevich@gmail.com>
2023-02-27 14:02:31 +0300

    Added new properties in class "Villa". Created new class "ApplicationDbContext". Get connection with DB in appsettings.json

---
+++ MagicVilla_VillaApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using MagicVilla_VillaApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_VillaApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Villa> Villas { get; set; }
+    }
+}
